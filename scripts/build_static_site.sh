90aafcc1155885eef7c82d78a5aa39350cc0778c
onlyforthesis <t111368544@ntut.org.tw>
2026-04-07 09:55:50 +0800

    fix(build): include all extracted CSS/JS in static site bundle
    The build script was only copying HTML + shared assets but missed all
    page-scoped CSS and JS files created during the refactoring.

--- scripts/build_static_site.sh
+++ scripts/build_static_site.sh
@@ -17,7 +17,13 @@ EOF
 
 cp shared.css "$DIST_DIR/"
 cp nav.js "$DIST_DIR/"
+
+# Homepage assets
 cp index.html "$DIST_DIR/"
+cp index.css index-live.css index-content.css index-mpnn.css "$DIST_DIR/"
+cp index.js index-ui.js index-live.js index-charts.js "$DIST_DIR/"
+
+# Secondary page HTML
 cp about_me.html "$DIST_DIR/"
 cp works.html "$DIST_DIR/"
 cp gene_ai.html "$DIST_DIR/"
@@ -25,6 +31,15 @@ cp ngs.html "$DIST_DIR/"
 cp thesis.html "$DIST_DIR/"
 cp report.html "$DIST_DIR/"
 cp interview_prep.html "$DIST_DIR/"
+
+# Secondary page CSS
+cp about_me.css works.css gene_ai.css ngs.css "$DIST_DIR/"
+cp report.css thesis.css interview_prep.css "$DIST_DIR/"
+
+# Secondary page JS
+cp about_me.js works.js gene_ai.js ngs.js "$DIST_DIR/"
+cp thesis.js interview_prep.js "$DIST_DIR/"
+
 cp README.md "$DIST_DIR/"
 cp demo_notebook.ipynb "$DIST_DIR/"
 
