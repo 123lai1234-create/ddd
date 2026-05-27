e55999f4c42d78acf8528779448d4ad512e6734f
JT Lai <jtlai0921@gmail.com>
2026-05-27 17:13:42 +0800

    更新歌單：10首歌曲

--- astro/public/music/music-list.js
+++ astro/public/music/music-list.js
@@ -1,15 +1,104 @@
 // Default playlist for music player
-// Add your music files here: /music/song.mp3
 
 const DEFAULT_PLAYLIST = [
     {
-        id: 'demo-1',
-        title: '我的音樂 1',
-        artist: '藝術家',
-        album: '專輯',
-        url: '/music/song1.mp3',
+        id: 'track-01',
+        title: '追光者',
+        artist: '岑寧兒',
+        album: '音樂專輯',
+        url: '/music/track-01-追光者---fc2a4520-78c6-4b07-b13b-9cb338bb2feb.mp3',
         cover: '',
-        duration: 180,
+        duration: 240,
+        lyrics: []
+    },
+    {
+        id: 'track-02',
+        title: '青春住了我',
+        artist: '楊丞琳',
+        album: '音樂專輯',
+        url: '/music/track-02-青春住了我---f0726dee-b260-4ec4-9b6a-2dd13c9c0113.mp3',
+        cover: '',
+        duration: 210,
+        lyrics: []
+    },
+    {
+        id: 'track-03',
+        title: '沙漠之心',
+        artist: '林俊傑',
+        album: '音樂專輯',
+        url: '/music/track-03-沙漠之心---23931c6e-8eb7-43dd-a9e5-e62e236c543c.mp3',
+        cover: '',
+        duration: 225,
+        lyrics: []
+    },
+    {
+        id: 'track-04',
+        title: '那些你很冒險的夢',
+        artist: '林俊傑',
+        album: '音樂專輯',
+        url: '/music/track-04-那些你很冒險的夢---fa0f3e04-93b3-4852-80ca-e8212e823b20.mp3',
+        cover: '',
+        duration: 255,
+        lyrics: []
+    },
+    {
+        id: 'track-05',
+        title: '說好的幸福呢',
+        artist: '周杰倫',
+        album: '音樂專輯',
+        url: '/music/track-05-說好的幸福呢---9a55310a-7278-4124-87dd-2c6d142a49d7.mp3',
+        cover: '',
+        duration: 240,
+        lyrics: []
+    },
+    {
+        id: 'track-06',
+        title: '給我一首歌的時間',
+        artist: '周杰倫',
+        album: '音樂專輯',
+        url: '/music/track-06-給我一首歌的時間---aa4ec65f-f1e1-4a04-8ddd-4a8338ac8243.mp3',
+        cover: '',
+        duration: 230,
+        lyrics: []
+    },
+    {
+        id: 'track-07',
+        title: '七里香',
+        artist: '周杰倫',
+        album: '音樂專輯',
+        url: '/music/track-07-七里香---65b662ae-f285-4631-83ca-a98c80e4db2c.mp3',
+        cover: '',
+        duration: 245,
+        lyrics: []
+    },
+    {
+        id: 'track-08',
+        title: '夜曲',
+        artist: '周杰倫',
+        album: '音樂專輯',
+        url: '/music/track-08-夜曲---78a16b19-acd6-4087-bfce-8c0069bcbf81.mp3',
+        cover: '',
+        duration: 235,
+        lyrics: []
+    },
+    {
+        id: 'track-09',
+        title: '南山南',
+        artist: '馬頔',
+        album: '音樂專輯',
+        url: '/music/track-09-南山南---1b3116c8-f5f6-4f36-b27e-b39d16315dba.mp3',
+        cover: '',
+        duration: 260,
+        lyrics: []
+    },
+    {
+        id: 'track-10',
+        title: '董小姐',
+        artist: '宋冬野',
+        album: '音樂專輯',
+        url: '/music/track-10-董小姐---771c5519-c626-4955-a42a-23c5b48cc4f7.mp3',
+        cover: '',
+        duration: 250,
         lyrics: []
     }
 ];
