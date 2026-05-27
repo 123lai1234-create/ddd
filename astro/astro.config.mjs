87762a8d3d7d652a86acceb33c989716a1d33650
JT Lai <jtlai0921@gmail.com>
2026-05-27 14:31:49 +0800

    fix: change Astro build format to directory for Vercel

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 export default defineConfig({
     integrations: [react()],
     build: {
-        format: 'file',
+        format: 'directory',
     },
     vite: {
         optimizeDeps: {
