b7c9b323adcb453f5daae3031d65eb7fbd2e194a
JT Lai <jtlai0921@gmail.com>
2026-05-27 17:08:38 +0800

    新增音樂播放器功能：歌詞、LRC動態同步、播放清單

---
+++ astro/public/music/lyrics/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26f5b648e96e56b652e555d84f0cec6453ba00c55eb0c416da76d5285285d473
+size 441
---
+++ astro/public/music/music-list.js
@@ -0,0 +1,17 @@
+// Default playlist for music player
+// Add your music files here: /music/song.mp3
+
+const DEFAULT_PLAYLIST = [
+    {
+        id: 'demo-1',
+        title: '我的音樂 1',
+        artist: '藝術家',
+        album: '專輯',
+        url: '/music/song1.mp3',
+        cover: '',
+        duration: 180,
+        lyrics: []
+    }
+];
+
+window.DEFAULT_PLAYLIST = DEFAULT_PLAYLIST;
