997db0d7217dc79b91356b9931b05607d6852d78
JT Lai <jtlai0921@gmail.com>
2026-06-01 14:26:18 +0800

    Trigger redeploy

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     build: {
-        format: 'file',
+        format: 'directory',
         assets: '_assets',
     },
     compressHTML: true,
